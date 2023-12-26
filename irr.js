6ae06dc6d18c5434d0ac748166aba43878294923
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2023-12-26 21:59:57 +0100

    Update irr.js

--- irr.js
+++ irr.js
@@ -1,8 +1,10 @@
-
 window.onload=()=>{
    
    let btn = document.querySelector("button") 
-    
+   let npvH = document.querySelector(".npvH")
+  let npvL = document.querySelector(".npvL")
+  let oldInt = document.querySelector(".oInt")
+  let newInt = document.querySelector("n.Int")
     btn.addEventListener("click",(e)=>{
         e.preventDefault()
     })
