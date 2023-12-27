bfa6105d1fc577a9418ec5d9c70c564a9c35fd1a
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2023-12-27 21:53:44 +0100

    Update irr.js

--- irr.js
+++ irr.js
@@ -1,13 +1,32 @@
 window.onload=()=>{
+
+/*
+IRR FORMULA 
+ Ra + (NPVa/NPVa-NPVb ) * (Ra - Rb)
+ where
+Ra = lower Disc Rate 
+Rb = higher Disc Rate
+NPVa = NPV at Ra
+NPVb = NPV at Rb
+
+*/
    
    let btn = document.querySelector("button") 
    let npvH = document.querySelector(".npvH")
   let npvL = document.querySelector(".npvL")
   let oldInt = document.querySelector(".oInt")
-  let newInt = document.querySelector("n.Int")
+  let newInt = document.querySelector(".nInt")
     btn.addEventListener("click",(e)=>{
         e.preventDefault()
     })
+    
+    let irr=(n)=>{
+        
+        
+        
+        
+    }
+    
     
     
     
